b707d124b61568339f6c73869ed7931e1b7a5a29
shirishmaharjan <maharjancris@gmail.com>
2026-05-26 16:51:35 +0545

    final stable version with name changes

--- constants/teams.ts
+++ constants/teams.ts
@@ -9,12 +9,11 @@ export const GROUPS_DATA: any = {
   H: { teams: [{ id: 'esp', n: 'Spain', c: 'es', r: 8, ch: 90 }, { id: 'uru', n: 'Uruguay', c: 'uy', r: 14, ch: 66 }, { id: 'ksa', n: 'Saudi Arabia', c: 'sa', r: 53, ch: 23 }, { id: 'cpv', n: 'Cape Verde', c: 'cv', r: 65, ch: 21 }] },
   I: { teams: [{ id: 'fra', n: 'France', c: 'fr', r: 2, ch: 83 }, { id: 'nor', n: 'Norway', c: 'no', r: 44, ch: 49 }, { id: 'sen', n: 'Senegal', c: 'sn', r: 17, ch: 38 }, { id: 'irq', n: 'Iraq', c: 'iq', r: 58, ch: 30 }] },
   J: { teams: [{ id: 'arg', n: 'Argentina', c: 'ar', r: 1, ch: 86 }, { id: 'aut', n: 'Austria', c: 'at', r: 25, ch: 57 }, { id: 'alg', n: 'Algeria', c: 'dz', r: 43, ch: 40 }, { id: 'jor', n: 'Jordan', c: 'jo', r: 71, ch: 17 }] },
-  K: { teams: [{ id: 'por', n: 'Portugal', c: 'pt', r: 6, ch: 78 }, { id: 'col', n: 'Colombia', c: 'co', r: 12, ch: 65 }, { id: 'cod', n: 'cd', r: 67, ch: 34 }, { id: 'uzb', n: 'uz', r: 64, ch: 23 }] },
+  K: { teams: [{ id: 'por', n: 'Portugal', c: 'pt', r: 6, ch: 78 }, { id: 'col', n: 'Colombia', c: 'co', r: 12, ch: 65 }, { id: 'cod', n: 'Congo DR', c: 'cd', r: 67, ch: 34 }, { id: 'uzb', n: 'Uzbekistan', c: 'uz', r: 64, ch: 23 }] },
   L: { teams: [{ id: 'eng', n: 'England', c: 'gb-eng', r: 4, ch: 87 }, { id: 'cro', n: 'Croatia', c: 'hr', r: 10, ch: 55 }, { id: 'gha', n: 'Ghana', c: 'gh', r: 61, ch: 39 }, { id: 'pan', n: 'Panama', c: 'pa', r: 45, ch: 19 }] },
 };
 
 export const BRACKET_MAPPING = [
-  // Round of 32
   { id: 'm73', t1: 'A1', t2: '3RD-1' }, { id: 'm74', t1: 'E1', t2: 'A2' },
   { id: 'm75', t1: 'F1', t2: 'C2' }, { id: 'm76', t1: 'B1', t2: '3RD-2' },
   { id: 'm77', t1: 'C1', t2: '3RD-3' }, { id: 'm78', t1: 'G1', t2: 'B2' },
@@ -23,18 +22,13 @@ export const BRACKET_MAPPING = [
   { id: 'm83', t1: 'L1', t2: '3RD-6' }, { id: 'm84', t1: 'J1', t2: 'H2' },
   { id: 'm85', t1: 'K1', t2: 'L2' }, { id: 'm86', t1: 'D2', t2: 'F2' },
   { id: 'm87', t1: 'B1', t2: '3RD-7' }, { id: 'm88', t1: 'K2', t2: '3RD-8' },
-  // Round of 16
   { id: 'm89', t1: 'W73', t2: 'W74' }, { id: 'm90', t1: 'W75', t2: 'W76' },
   { id: 'm91', t1: 'W77', t2: 'W78' }, { id: 'm92', t1: 'W79', t2: 'W80' },
   { id: 'm93', t1: 'W81', t2: 'W82' }, { id: 'm94', t1: 'W83', t2: 'W84' },
   { id: 'm95', t1: 'W85', t2: 'W86' }, { id: 'm96', t1: 'W87', t2: 'W88' },
-  // Quarters
   { id: 'm97', t1: 'W89', t2: 'W90' }, { id: 'm98', t1: 'W91', t2: 'W92' },
   { id: 'm99', t1: 'W93', t2: 'W94' }, { id: 'm100', t1: 'W95', t2: 'W96' },
-  // Semis
   { id: 'm101', t1: 'W97', t2: 'W98' }, { id: 'm102', t1: 'W99', t2: 'W100' },
-  // 3rd Place Match
   { id: 'm103', t1: 'L101', t2: 'L102' },
-  // Final
   { id: 'm104', t1: 'W101', t2: 'W102' },
 ];
